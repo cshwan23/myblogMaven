0f04974682bab5f6902efb920c29d1a23711adb7
cshwan23 <shchoigo0@gmail.com>
2021-07-29 21:37:31 +0900

    Initial commit

---
+++ src/main/java/com/example/myblogGradle/MyblogGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.myblogGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myblogGradle/MyblogGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myblogGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
